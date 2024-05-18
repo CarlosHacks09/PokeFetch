b9afef9d01c2052b7f65c12970c7c58ac2e7da3a
CarlosHacks09 <carlossantiago.ramirez@liceodelvalle.edu.mx>
2024-05-18 14:24:04 -0600

    modified:   index.css 	modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 const tarjetas = $(".tarjetas");
+// https://pokeapi.co/api/v2/pokemon?limit=20&offset=20
 
-async function fetchData() {
-    const data = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
+const pokemon_limit = 20;
+
+async function fetchManyPokemons(limit) {
+    const offset = Math.floor(Math.random() * (1302 - limit));
+
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${pokemon_limit}&offset=${offset}`);
     const json_data = await data.json();
-    const nombre = json_data.name
-    const url_img = json_data.sprites.other.dream_world.front_default
-    const tipo = json_data.types[0].type.name
-    const habilidades = ["", ""]
-    habilidades[0] = json_data.abilities[0].ability.name
-    habilidades[1] = json_data.abilities[1].ability.name
-    const progreso = json_data.stats[0].base_stat
-    createCard(tarjetas, nombre, url_img, tipo, habilidades, progreso)
+    json_data.results.forEach(async (pokemon) => await fetchData(pokemon.name));
 }
 
-fetchData();
+fetchManyPokemons(pokemon_limit);
+
+async function fetchData(pokemon_name) {
+    try {
+        const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_name}`);
+        const json_data = await data.json();
+    
+        const nombre = json_data.name
+        const url_img = json_data.sprites?.other?.dream_world?.front_default ?? "https://t3.ftcdn.net/jpg/03/35/13/14/360_F_335131435_DrHIQjlOKlu3GCXtpFkIG1v0cGgM9vJC.jpg"
+        const tipo = json_data.types[0].type.name
+        const habilidades = ["", ""]
+        habilidades[0] = json_data.abilities[0]?.ability?.name ?? "No ability";
+        habilidades[1] = json_data.abilities[1]?.ability?.name ?? "No ability";
+        const progreso = json_data.stats[0].base_stat
+        createCard(tarjetas, nombre, url_img, tipo, habilidades, progreso)
+    } catch (err) {
+        alert(err);
+    }
+}
 
 function createCard(contenedor, nombre, url_img, tipo, habilidades, progreso) {
-    contenedor.append(`
+    contenedor.prepend(`
         <div class="tarjeta">
             <h3 class="nombre">${nombre}</h3>
             <div class="imgcon">
@@ -35,3 +51,18 @@ function createCard(contenedor, nombre, url_img, tipo, habilidades, progreso) {
 }
 
 
+const input = $("#search");
+const buscar = $("#buscar");
+
+input.on("keydown", (event) => {
+    if (event.keyCode == 13) {
+        console.log("Buscar pokemon")
+    } else {
+        console.log("No buscar")
+    }
+
+});
+
+buscar.on("click", async () => {
+    await fetchData(input[0].value)
+});
