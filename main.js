03748cf07f046ba424eed81086896a7140fa53e3
CarlosHacks09 <carlossantiago.ramirez@liceodelvalle.edu.mx>
2024-05-21 16:56:20 -0600

    Added search function and error warnings :)

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const tarjetas = $(".tarjetas");
-// https://pokeapi.co/api/v2/pokemon?limit=20&offset=20
+const popup = $(".popup")[0];
+const button = $(".button");
+const errmsg = $("#errmsg")[0];
 
-const pokemon_limit = 20;
+console.log(button)
+
+const pokemon_limit = 1302;
 
 async function fetchManyPokemons(limit) {
-    const offset = Math.floor(Math.random() * (1302 - limit));
+    const offset = 0;
 
     const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${pokemon_limit}&offset=${offset}`);
     const json_data = await data.json();
@@ -13,11 +17,11 @@ async function fetchManyPokemons(limit) {
 
 fetchManyPokemons(pokemon_limit);
 
-async function fetchData(pokemon_name) {
+async function fetchData(pokemon_name, append = true) {
     try {
         const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_name}`);
         const json_data = await data.json();
-    
+
         const nombre = json_data.name
         const url_img = json_data.sprites?.other?.dream_world?.front_default ?? "https://t3.ftcdn.net/jpg/03/35/13/14/360_F_335131435_DrHIQjlOKlu3GCXtpFkIG1v0cGgM9vJC.jpg"
         const tipo = json_data.types[0].type.name
@@ -25,44 +29,67 @@ async function fetchData(pokemon_name) {
         habilidades[0] = json_data.abilities[0]?.ability?.name ?? "No ability";
         habilidades[1] = json_data.abilities[1]?.ability?.name ?? "No ability";
         const progreso = json_data.stats[0].base_stat
-        createCard(tarjetas, nombre, url_img, tipo, habilidades, progreso)
+        createCard(tarjetas, nombre, url_img, tipo, habilidades, progreso, append)
     } catch (err) {
-        alert(err);
+        popup.className = "popup";
+        errmsg.innerText = err;
+        setTimeout(() => {
+            popup.className = "hidden";
+        }, 3500);
+
     }
 }
 
-function createCard(contenedor, nombre, url_img, tipo, habilidades, progreso) {
-    contenedor.prepend(`
-        <div class="tarjeta">
-            <h3 class="nombre">${nombre}</h3>
-            <div class="imgcon">
-                <img class="img"
-                    src="${url_img}"
-                    alt="">
-            </div>
-            <h4 class="tipo">${tipo}</h4>
-            <div class="habilidad">
-                <h5>${habilidades[0]}</h5>
-                <h5>${habilidades[1]}</h5>
-            </div>
-            <progress class="bar" id="file" max="100" value="${progreso}"></progress>
-        </div> 
-    `);
+function createCard(contenedor, nombre, url_img, tipo, habilidades, progreso, append = true) {
+    append ?
+        contenedor.append(`
+            <div class="tarjeta">
+                <h3 class="nombre">${nombre}</h3>
+                <div class="imgcon">
+                    <img class="img"
+                        src="${url_img}"
+                        alt="">
+                </div>
+                <h4 class="tipo">${tipo}</h4>
+                <div class="habilidad">
+                    <h5>${habilidades[0]}</h5>
+                    <h5>${habilidades[1]}</h5>
+                </div>
+                <progress class="bar" id="file" max="100" value="${progreso}"></progress>
+            </div> 
+        `) :
+        contenedor.prepend(`
+            <div class="tarjeta">
+                <h3 class="nombre">${nombre}</h3>
+                <div class="imgcon">
+                    <img class="img"
+                        src="${url_img}"
+                        alt="">
+                </div>
+                <h4 class="tipo">${tipo}</h4>
+                <div class="habilidad">
+                    <h5>${habilidades[0]}</h5>
+                    <h5>${habilidades[1]}</h5>
+                </div>
+                <progress class="bar" id="file" max="100" value="${progreso}"></progress>
+            </div> 
+        `);
 }
 
 
 const input = $("#search");
 const buscar = $("#buscar");
 
-input.on("keydown", (event) => {
+input.on("keydown", async (event) => {
     if (event.keyCode == 13) {
-        console.log("Buscar pokemon")
+        console.log(input[0].value)
+        await fetchData(input[0].value, false)
     } else {
         console.log("No buscar")
     }
 
 });
 
-buscar.on("click", async () => {
-    await fetchData(input[0].value)
+button.on("click", () => {
+    popup.className = "hidden";
 });
